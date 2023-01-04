1ca6aa58e8a1b7351274d49a1844ea144eb5c1b4
Austin Schonfeld <austinschonfeld@gmail.com>
2023-01-03 21:17:59 -0700

    Attempting to make a grouped bar chart of genres and ratings

--- netflix.py
+++ netflix.py
@@ -34,6 +34,7 @@ def breakdown(column):
     new_frame = data[['show_id', column]]
     return new_frame
 
+#use the breakdown function to get tables for each column
 types = breakdown('type')
 titles = breakdown('title')
 directors = breakdown('director')
@@ -98,4 +99,17 @@ plt.ylabel('Number of Shows')
 plt.savefig('show_lengths.png')
 plt.clf()
 
-print(show_durations.head(25))
+#lets get the distribution of ratings for each genre
+#combine ratings with categories
+ratings_by_genre = ratings.merge(categories, on = 'show_id')
+#remove the typos that the code on line 110 reveals
+ratings_by_genre = ratings_by_genre.drop(ratings_by_genre[(ratings_by_genre.rating == '66 min') | (ratings_by_genre.rating == '74 min') | (ratings_by_genre.rating == '84 min')].index)
+#now to get the number of shows per rating per category
+ratings_by_genre = ratings_by_genre.groupby(['listed_in', 'rating']).rating.count()
+#unstack the data into a more usable table
+unstacked_ratings = ratings_by_genre.unstack()
+#unstacked data revealed some typos where there is a movie duration in place of a rating, go back above and remove the rows containing 66, 74, and 84 min for ratings
+print(unstacked_ratings.head(20))
+#there are too many categories to do a single grouped bar chart, lets do a different chart for each category
+unstacked_ratings.plot(kind='bar')
+plt.savefig(('genres_and_ratings.png'))
